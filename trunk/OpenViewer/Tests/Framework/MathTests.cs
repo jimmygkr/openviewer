f719cdecb4c3672dca0af780ab3d8e8c352f8f6b
daedius@daedius.com <daedius@daedius.com@faa07838-f747-0410-81ff-5bdf4d21b671>
2008-03-09 02:55:46 +0000

    - adding nunit

---
+++ trunk/OpenViewer/Tests/Framework/MathTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+namespace OpenViewer.Framework.Tests
+{
+  [TestFixture]
+  public class MathTests
+  {
+    [Test]
+    public void Test1()
+    {
+      Assert.IsTrue(true);
+    }
+  }
+}
